f46cb79ea751c024eacb9629d38dca59aec344ee
Erik <erik@montnemery.com>
2023-10-14 12:11:24 +0200

    Rename improv_ble_client.py to client.py

--- improv_ble_client/__init__.py
+++ improv_ble_client/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from . import errors
-from .improv_ble_client import ImprovBLEClient, device_filter
+from .client import ImprovBLEClient, device_filter
 from .protocol import Error, State
 
 __all__ = [
